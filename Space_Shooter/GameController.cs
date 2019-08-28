3264389c3208c1fd350a7fbd411c5d75231667e7
oudrew <34745622+oudrew@users.noreply.github.com>
2019-08-28 10:05:29 -0500

    Add files via upload

---
+++ Space_Shooter/GameController.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Text scoreText;
+    public Text waveText;
+    public Text restartText;
+    public Text gameOverText;
+    public Text waveFlashText;
+    public Text lifeText;
+    public Text endScoreText;
+    private int score;
+    private int wave;
+    int wave2;
+    public int lifeCount;
+    private bool gameOver;
+    private bool restart;
+
+    void Start()
+    {
+        score = 0;
+        wave = 1;
+        wave2 = 0;
+        lifeCount = 3;
+        updateScore();
+        updateWave();
+        updateLives();
+        StartCoroutine (SpawnWaves());
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        waveFlashText.text = "";
+        endScoreText.text = "";
+    }
+	
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown (KeyCode.R))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+    IEnumerator SpawnWaves()
+    {
+        while (true)
+        {
+            waveFlashText.text = "";
+            for (int i = 0; i < hazardCount; i++)
+            {
+                yield return new WaitForSeconds(startWait);
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+                if (gameOver)
+                {
+                    restartText.text = "Press 'R' to restart";
+                    restart = true;
+                    break;
+                }
+            }
+            if (gameOver)
+            {
+                break;
+            }
+            yield return new WaitForSeconds(waveWait);
+            addWave();
+            yield return new WaitForSeconds(waveWait);
+            if (wave2 > 1)
+            {
+                spawnWait = spawnWait / 2;
+                wave2 = 0;
+            }
+        }
+    }
+
+    void updateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    void updateWave()
+    {
+        waveText.text = "Wave : " + wave;
+        waveFlashText.text = "Wave " + wave;
+    }
+
+    public void updateLives()
+    {
+        lifeText.text = "Lives: " + lifeCount;
+    }
+
+    public void loseLife()
+    {
+        lifeCount--;
+    }
+
+    public void AddScore(int NewScoreValue)
+    {
+        score += NewScoreValue;
+        updateScore();
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game over";
+        endScoreText.text = "Your score: " + score;
+        gameOver = true;
+    }
+    public void addWave()
+    {
+        wave++;
+        wave2++;
+        hazardCount++;
+        updateWave();
+    }
+}
---
+++ Space_Shooter/SpacePlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class SpacePlayerController : MonoBehaviour
+{
+    public Boundary boundary;
+    public float tilt;
+    private Rigidbody rb;
+    public float speed;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            nextFire = Time.time + fireRate;
+            audioSource.Play();
+        }
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3(Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax), 
+            0, Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax));
+        rb.rotation = Quaternion.Euler(0, 0, rb.velocity.x * -tilt);
+    } 
+}
